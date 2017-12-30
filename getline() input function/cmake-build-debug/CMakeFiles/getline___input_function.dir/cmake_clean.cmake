f59f86ab0d38d13f26c88522b85c9a49672a2d01
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2017-12-30 19:13:43 +0200

    input variant

---
+++ getline() input function/cmake-build-debug/CMakeFiles/getline___input_function.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/getline___input_function.dir/main.cpp.o"
+  "getline___input_function.pdb"
+  "getline___input_function"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/getline___input_function.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
