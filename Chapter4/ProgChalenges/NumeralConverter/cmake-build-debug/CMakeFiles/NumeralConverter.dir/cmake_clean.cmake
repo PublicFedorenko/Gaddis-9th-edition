019238446e676de888ceeb23b92f1af01632a9ff
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-01-07 21:58:15 +0200

    new chapter

---
+++ Chapter4/ProgChalenges/NumeralConverter/cmake-build-debug/CMakeFiles/NumeralConverter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NumeralConverter.dir/main.cpp.o"
+  "NumeralConverter.pdb"
+  "NumeralConverter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NumeralConverter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Chapter4/ProgChalenges/NumeralConverter/cmake-build-debug/CMakeFiles/NumeralConverter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NumeralConverter.
+# This may be replaced when dependencies are built.
