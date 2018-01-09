3b655d5c718d295306270bd8a4f25bbbfc76c2bf
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-01-09 23:47:22 +0200

    new chalenge

---
+++ Chapter4/ProgChalenges/BookClubPoints/cmake-build-debug/CMakeFiles/BookClubPoints.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BookClubPoints.dir/main.cpp.o"
+  "BookClubPoints.pdb"
+  "BookClubPoints"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BookClubPoints.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
