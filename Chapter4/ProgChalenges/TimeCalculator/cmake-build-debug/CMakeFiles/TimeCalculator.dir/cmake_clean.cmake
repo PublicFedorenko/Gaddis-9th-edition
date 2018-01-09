0b4c8feee60952542fd9b80a451d37eeef8fd712
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-01-09 23:49:08 +0200

    new chalenge

---
+++ Chapter4/ProgChalenges/TimeCalculator/cmake-build-debug/CMakeFiles/TimeCalculator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TimeCalculator.dir/main.cpp.o"
+  "TimeCalculator.pdb"
+  "TimeCalculator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TimeCalculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
