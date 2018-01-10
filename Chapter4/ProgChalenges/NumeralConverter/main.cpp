e67da492aed69b340742f6f3f9ba9f78c7df2784
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-01-10 22:34:27 +0200

    rewrited Roman Numeral Converter

--- Chapter4/ProgChalenges/NumeralConverter/main.cpp
+++ Chapter4/ProgChalenges/NumeralConverter/main.cpp
@@ -1,6 +1,28 @@
+/*
+ * Write a program that asks the user to enter a number within the range of 1 through 10.
+ * Use a switch statement to display the Roman numeral version of that number.
+ */
 #include <iostream>
+#include <string>
+using namespace std;
+
+#define SIZE 10
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const unsigned int charsToIgnore = 100;
+    int number;
+    string romanNumbers[SIZE] = {"I", "II", "III", "IIII", "V", "VI", "VII", "VIII", "VIIII", "X"};
+
+    cout << "Enter a number within the range of 1 trough 10: ";
+    cin >> number;
+    while (number < 1 || number > 10) {
+        cout << "Your number must be within range of 1 through 10: ";
+        cin.ignore(charsToIgnore, '\n');
+        cin >> number;
+        cout << endl;
+    }
+
+    cout << "Number " << number << " is " << romanNumbers[number - 1] << " in Roman numeral system";
+
     return 0;
 }
