51f8fcf6e0aa50de3a835a160f8a5e14252fa2f8
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-01-11 14:13:27 +0200

    added implementation

--- Chapter4/ProgChalenges/TimeCalculator/main.cpp
+++ Chapter4/ProgChalenges/TimeCalculator/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+using namespace std;
+
+#define SEC_IN_MIN 60
+#define SEC_IN_HOUR 3600
+#define SEC_IN_DAY 86400
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int seconds;
+
+    cout << "Enter number of seconds: ";
+    cin >> seconds;
+    while (seconds < 60) {
+        cout << "Seconds must be grater than or equal to 60.";
+        cin >> seconds;
+        cout << endl;
+    }
+
+    if (seconds >= SEC_IN_DAY) {
+        cout << seconds << " seconds is " << seconds / SEC_IN_DAY << " days.\n";
+    } else if (seconds >= SEC_IN_HOUR && seconds < SEC_IN_DAY) {
+        cout << seconds << " seconds is " << seconds / SEC_IN_HOUR << " hours.\n";
+    } else if (seconds >= SEC_IN_MIN && seconds < SEC_IN_HOUR) {
+        cout << seconds << " seconds is " << seconds / SEC_IN_MIN << " minutes.\n";
+    }
+
     return 0;
 }
