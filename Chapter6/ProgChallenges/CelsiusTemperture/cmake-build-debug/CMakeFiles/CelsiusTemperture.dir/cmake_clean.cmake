84216050df0fe85433e638f5fc1c7eec7d138e89
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-01-11 21:32:15 +0200

    CelsiusTemperture implementation added

---
+++ Chapter6/ProgChallenges/CelsiusTemperture/cmake-build-debug/CMakeFiles/CelsiusTemperture.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CelsiusTemperture.dir/main.cpp.o"
+  "CelsiusTemperture.pdb"
+  "CelsiusTemperture"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CelsiusTemperture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Chapter6/ProgChallenges/CelsiusTemperture/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+#define MIN_FAHRENHEIT 0
+#define MAX_FAHRENHEIT 20
+
+double celsius(double);
+
+int main() {
+    cout << "Fahrenheit         Celsius\n";
+    cout << "_________________________\n";
+    for (int fahrenheit = MIN_FAHRENHEIT; fahrenheit <= MAX_FAHRENHEIT; ++fahrenheit) {
+        cout << fahrenheit << "                " << right << celsius(fahrenheit) << endl;
+    }
+    return 0;
+}
+
+double celsius(double f) {
+    double cel = ((double)5/9)*(f - 32);
+    return cel;
+}
