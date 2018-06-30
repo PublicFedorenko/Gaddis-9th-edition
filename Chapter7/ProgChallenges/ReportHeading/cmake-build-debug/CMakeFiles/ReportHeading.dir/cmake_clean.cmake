6dce1f56647ecd5221d4da763fe08d80c5190d36
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-06-30 13:46:58 +0300

    empty project

---
+++ Chapter7/ProgChallenges/ReportHeading/cmake-build-debug/CMakeFiles/ReportHeading.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReportHeading.dir/main.cpp.o"
+  "ReportHeading.pdb"
+  "ReportHeading"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReportHeading.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
